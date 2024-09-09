cf63f3572cfe841cc862c4c08737530215d636ef
LavenderLO <lolavender528@gmail.com>
2024-09-09 16:35:03 -0400

    Created new Java classes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    // type psvm and press enter to accept the autocomplete and it will generate an empty public static void main for us
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(args);
+}
+}
