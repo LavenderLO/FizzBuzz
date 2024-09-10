3a374380aca648a8276b76c57db25c3f596d7e3b
LavenderLO <lolavender528@gmail.com>
2024-09-10 01:13:50 -0400

    Finished tasks 'multiples' and 'reduce'

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,5 +1,42 @@
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println();
+        int count = 0;
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0 || i % 5 == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
+
+
+/* The AND operator is the double ampersand &&.
+The OR operator is the double pipe ||.
+The NOT operator is the exclaimation !.
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0 || i % 5 == 0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+public static void main(String[] args) {
+        int count = 0;
+        int i = 1;
+        while (i < 1000) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+            i++;
+        }
+        System.out.println(count);
+    }
+}
+ */
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,38 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n--;
+            }
+            steps++;
+        }
+        System.out.println("answer is" + steps);
+    }
 }
+
+/*
+
+Math.pow( base, exponent ) - Perform exponential calculation, base raised to exponent.
+Math.sqrt( num ) - What is the square root of num?
+Math.round( num ) - Round num to the nearest integer value
+Math.floor( num ) - Truncate any decimals off of num down to the nearest integer value
+Math.abs( num ) - What is the absolutele value of num?
+Math.random() - Generate a random number between 0 and 1
+Math.max( a, b ) - Which number is larger out of a or b?
+Math.min( a, b ) - Which number is smaller out of a or b?
+
+A while/ for loop that counts from 0 to 9:
+int a = 0;
+while (a < 10){
+    system.out.println( a );
+    a = a + 1
+
+for (int i=0; i<10; i=i+1){
+    system.out.println( i );
+}
+
+ */
